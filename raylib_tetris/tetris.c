27bc8ce7b4e6c4bb21e917db0e9bd083c5972464
fbottarelli <f.bottarelli@miriade.it>
2024-04-21 12:37:38 +0200

    fix grid dimension

--- raylib_tetris/tetris.c
+++ raylib_tetris/tetris.c
@@ -10,34 +10,40 @@ int main(void)
     //--------------------------------------------------------------------------------------
     const int screenWidth = 400;
     const int screenHeight = 800;
+    float tetraminosRadius = 20*sqrt(2);
+    const int squareSide = 40;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-    // first block start position
-    Vector2 tetraminosPosition = { (float)40, (float)40 };
+    
+    // spawn position
+    int tetraminosSpawnPosition[1] = { (int)squareSide/2, (int)squareSide/2 };
+    
+    // position of moving tetraminus
+    // Vector2 tetraminosPosition = { tetraminosRadius/2, tetraminosRadius/2 };
+    // Vector2 tetraminosPosition = { squareSide/2, squareSide/2 };
+    Vector2 tetraminosPosition = { 0, 0 };
 
-    SetTargetFPS(15);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
-    float tetraminosRadius = 40;
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
         // move the tetraminos
         //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT) && tetraminosPosition.x <= screenWidth - tetraminosRadius ) { 
+        if (IsKeyPressed(KEY_RIGHT) && tetraminosPosition.x <= screenWidth - squareSide ) { 
             tetraminosPosition.x += 40.0f; 
             }
-        if (IsKeyDown(KEY_LEFT) && tetraminosPosition.x >= tetraminosRadius) { 
+        if (IsKeyPressed(KEY_LEFT) && tetraminosPosition.x >= squareSide) { 
             tetraminosPosition.x -= 40.0f; 
             }
         // if (IsKeyDown(KEY_UP) && tetraminosPosition.y >= 0) { 
         //     tetraminosPosition.y -= 40.0f; 
         //     }
-        if (IsKeyDown(KEY_DOWN) && tetraminosPosition.y <= screenHeight - tetraminosRadius) { 
+        if (IsKeyPressed(KEY_DOWN) && tetraminosPosition.y <= screenHeight - squareSide) { 
             tetraminosPosition.y += 40.0f;
             }
-        //----------------------------------------------------------------------------------
-
+        //--------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -45,16 +51,16 @@ int main(void)
 
             ClearBackground(RAYWHITE);
             // Draw grid
-            for (int x = 0; x < screenWidth; x += 50) {
+            for (int x = 0; x < screenWidth; x += screenWidth/10) {
                 DrawLine(x, 0, x, screenHeight, BLACK);
             }
-            for (int y = 0; y < screenHeight; y += 50) {
+            for (int y = 0; y < screenHeight; y += screenHeight/20) {
                 DrawLine(0, y, screenWidth, y, BLACK);
             }
 
             DrawText("move the tetraminos with arrow keys", 50, (float)screenHeight/2, 19, DARKGRAY);
 
-            DrawPoly(tetraminosPosition, 4, tetraminosRadius, 45, BROWN);
+            DrawRectangle(tetraminosPosition.x, tetraminosPosition.y, squareSide,squareSide, BROWN);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
